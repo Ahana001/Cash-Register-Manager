1aea94084c2bdc81a13543cf9926f30f8c9b1994
Ankita <thakkarankita0105@.github.com>
2022-01-27 23:10:55 +0530

    cash register manager

--- app.js
+++ app.js
@@ -2,22 +2,34 @@ var billAmount = document.querySelector("#bill-amount");
 var cashGiven = document.querySelector("#cash-given");
 var message = document.querySelector("#message");
 var checkButton = document.querySelector("#checkButton");
+var notes = document.querySelectorAll(".note");
+var NoteArray = [2000,500,100,20,10,5,1];
 
-function validateBillAmount() {
+function calculateNoOfteNotes(value){
+    for(let i=0; i<NoteArray.length; i++)
+    {
+        var NoOfNote = Math.trunc(value/NoteArray[i]);
+        notes[i].innerText = NoOfNote;
+        value = value % NoteArray[i];
+    }
+}
+
+function validateBillAmount(bill,GivenAmount) {
     message.style.display = "none";
-    if (billAmount.value <= 0) {
+    if (bill <= 0) {
         message.style.display = "block";
         message.innerText = "Bill Amount must be greater than 0";
     } else {
-        if (cashGiven.value < billAmount.value) {
+        if (parseInt(GivenAmount) < parseInt(bill)) {
             message.style.display = "block";
             message.innerText = "cash given must be greater than bill amount";
         } else {
-            console.log("Hello");
+            var ReturnAmount = GivenAmount-bill;
+            calculateNoOfteNotes(ReturnAmount);
         }
     }
 }
 
 checkButton.addEventListener('click', () => {
-    validateBillAmount();
+    validateBillAmount(billAmount.value,cashGiven.value);
 });
